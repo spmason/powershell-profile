180fc0fb68b60831c5d224493a7c3144eb519a0d
unknown <Steve Mason@.(none)>
2010-04-10 15:06:03 +0100

    Added PoshGit bits & common profile

---
+++ profile.ps1
@@ -0,0 +1,60 @@
+Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+# Git utils
+. ./GitUtils.ps1
+. ./GitPrompt.ps1
+
+# Use Git tab expansion
+. ./GitTabExpansion.ps1
+
+Pop-Location
+
+# Set up a simple prompt, adding the git prompt parts inside git repos
+function prompt {
+    Write-Host($pwd) -nonewline
+        
+    # Git Prompt
+    $Global:GitStatus = Get-GitStatus
+    Write-GitStatus $GitStatus
+      
+    return "> "
+}
+
+if(-not (Test-Path Function:\DefaultTabExpansion)) {
+    Rename-Item Function:\TabExpansion DefaultTabExpansion
+}
+
+# Set up tab expansion and include git expansion
+function TabExpansion($line, $lastWord) {
+    $lastBlock = [regex]::Split($line, '[|;]')[-1]
+    
+    switch -regex ($lastBlock) {
+        # Execute git tab completion for all git-related commands
+        'git (.*)' { GitTabExpansion $lastBlock }
+        # Fall back on existing tab expansion
+        default { DefaultTabExpansion $line $lastWord }
+    }
+}
+
+Enable-GitColors
+
+Pop-Location
+
+# Define aliases
+function gitStatus{
+	git status
+}
+set-alias gs gitStatus
+
+function cdDev{
+	cd E:\_Development
+}
+set-alias dev cdDev
+
+function openNotepad{
+	notepad $args
+}
+set-alias n openNotepad
+
+$dropbox = 'C:\Users\Steve Mason\Documents\My Dropbox'
+$dev = 'e:\_Development'
