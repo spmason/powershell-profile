ebe884e39a32e6ef9e5a68b4c908aa9d1325651b
unknown <Steve Mason@.(none)>
2010-04-10 15:26:07 +0100

    Profile now loads the posh-git profile script, environment.ps1 added as an example

---
+++ environment.ps1
@@ -0,0 +1,2 @@
+$dropbox = 'C:\Users\Steve Mason\Documents\My Dropbox'
+$dev = 'e:\_Development'
--- profile.ps1
+++ profile.ps1
@@ -1,48 +1,10 @@
 Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-# Git utils
-. ./GitUtils.ps1
-. ./GitPrompt.ps1
-
-# Use Git tab expansion
-. ./GitTabExpansion.ps1
-
-Pop-Location
-
-# Set up a simple prompt, adding the git prompt parts inside git repos
-function prompt {
-    Write-Host($pwd) -nonewline
-        
-    # Git Prompt
-    $Global:GitStatus = Get-GitStatus
-    Write-GitStatus $GitStatus
-      
-    return "> "
-}
-
-if(-not (Test-Path Function:\DefaultTabExpansion)) {
-    Rename-Item Function:\TabExpansion DefaultTabExpansion
-}
-
-# Set up tab expansion and include git expansion
-function TabExpansion($line, $lastWord) {
-    $lastBlock = [regex]::Split($line, '[|;]')[-1]
-    
-    switch -regex ($lastBlock) {
-        # Execute git tab completion for all git-related commands
-        'git (.*)' { GitTabExpansion $lastBlock }
-        # Fall back on existing tab expansion
-        default { DefaultTabExpansion $line $lastWord }
-    }
-}
-
-Enable-GitColors
-
-Pop-Location
+# Run posh-git init script
+. .\profile.example.ps1
 
 # Define variables
-$dropbox = 'C:\Users\Steve Mason\Documents\My Dropbox'
-$dev = 'e:\_Development'
+. .\environment.ps1
 
 # Define aliases
 function gitStatus{
@@ -64,3 +26,5 @@ function openNotepad{
 	notepad $args
 }
 set-alias n openNotepad
+
+Pop-Location
