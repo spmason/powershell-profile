490c16db019133eca23e0a4258295470b46cf4e7
Steve Mason <steve.mason@cubeworks.co.uk>
2010-07-29 15:41:20 +0100

    Fixed CassiniDev script

--- util/cassinidev.ps1
+++ util/cassinidev.ps1
@@ -17,7 +17,30 @@ function Start-Cassini{
 	push-location
 	
 	cd $CassiniDev
-	.\CassiniDev4.exe /a:$location
+	.\CassiniDev.exe /a:$location
+	
+	pop-location
+}
+function Start-Cassini4{
+	param([string]$location)
+	
+	if([System.String]::IsNullOrEmpty($location)){
+		$location = gl
+	}
+	
+	if($location.StartsWith(".\")){
+		$location = $location.TrimStart('.', '\', '/')
+	}
+	
+	if([System.IO.Path]::IsPathRooted($location) -eq $false){
+		$gl = gl
+		$location = [System.IO.Path]::Combine($gl, $location)
+	}
+	
+	push-location
+	
+	cd $CassiniDev
+	.\CassiniDev.exe /a:$location
 	
 	pop-location
 }
